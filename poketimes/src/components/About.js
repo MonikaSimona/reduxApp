a683ddffd7fe6be8c2526202f2dd846a7e34f859
MonikaSimona <simonovska_simona@outlook.com>
2021-03-16 10:29:26 +0100

    inital commit

---
+++ poketimes/src/components/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <div>
+            <div className="container">
+                <h4 className="center">
+                    About
+                </h4>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis, soluta?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut tempore excepturi recusandae sunt minima qui culpa blanditiis cumque, unde dicta?</p>
+            </div>
+           
+        </div>
+    )
+}
